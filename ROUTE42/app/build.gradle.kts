30b3bd1c69faa9329c36ca1c63e4ff722b2575d2
sj42tech <sj42tech@users.noreply.github.com>
2026-04-13 19:22:15 +0300

    Bump version to 0.1.5

--- ROUTE42/app/build.gradle.kts
+++ ROUTE42/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "io.github.sj42tech.route42"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
